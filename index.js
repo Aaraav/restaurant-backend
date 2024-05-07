f564a9269e73ab7c0e302aed433d6cdfd541156f
Aaraav <aaraav2810@gmail.com>
2024-05-07 19:23:12 +0530

    stage

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const app=express();
 const mainRoutes=require('./routes/main')
 
 const foodModel=require('./models/foodModel')
-app.use(cors({
-    origin:'https://restaurantaaraav.netlify.app/'
-}));
+app.use(cors());
 app.use(express.json());
 const bcyrpt=require('bcrypt');
 const jwt = require('jsonwebtoken');
